a0edc02555a80b1b1689785697ce64c366d5fa1a
TanJuniorr <letrungtinh.010178@gmail.com>
2026-05-17 21:49:07 +0700

    Add Week 6 and Week7

--- Week5/React-Css/Bai6/src/contexts/AuthContext.jsx
+++ Week5/React-Css/Bai6/src/contexts/AuthContext.jsx
@@ -19,5 +19,5 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
-
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => useContext(AuthContext);
---
+++ Week7/React-Css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Tuan7 from "./Tuan7";
+
+function App() {
+  return (
+    <div>
+      <Tuan7 />
+    </div>
+  );
+}
+
+export default App;
